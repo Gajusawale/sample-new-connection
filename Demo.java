f7c8c4626712adb615a80035b84de9cd56b69c29
Gajusawale <sawalegajanan1997@gmail.com>
2023-10-14 18:12:12 +0530

    first commit

---
+++ Demo.java
@@ -0,0 +1,13 @@
+import java.util.Date;
+
+public class Demo{
+
+public static void main(String args[])
+{
+	System.out.println("Hello World");
+
+	System.out.println("This output from java program");
+	System.out.println(new Date());
+	Sysetm.out.println("new Changes");
+}
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+"# simple_java_program" 
+# sample-new-connection
